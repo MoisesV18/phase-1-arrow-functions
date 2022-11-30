855f5a041106467d43bb06bcb372130be8345420
Moises Victoria <moisesvc1815@gmail.com>
2022-11-30 09:05:35 -0800

    commit tree 805783abf169edf0687f3609ea237e4a7e6f1783

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide() {
+    return divide = 2000/100
+}
+const square = (parameter1) => {
+    return parameter1 * parameter1
+}
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2
+}
